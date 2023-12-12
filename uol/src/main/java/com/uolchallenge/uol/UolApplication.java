18cc9e7be7872853e33a8e96b603e8f1b0c7b23f
Kauê Matos <kaue178hotimail@gmail.com>
2023-12-11 21:48:44 -0300

    fix:commit start challenge

---
+++ uol/src/main/java/com/uolchallenge/uol/UolApplication.java
@@ -0,0 +1,12 @@
+package com.uolchallenge.uol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UolApplication.class, args);
+	}
+}
